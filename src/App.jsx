66354d39b9d8ddbb1cbc4dfe26eab66a2f6d82a4
unknown <leventeg6@gmail.com>
2025-06-15 14:22:15 +0200

    part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Search from './components/Search.jsx'
+import {useState} from 'react';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   return(
-    'hello'
+    <main>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+    </main>
   );
 }
 
